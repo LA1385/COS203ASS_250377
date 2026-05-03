c0424e268696fba4ce48b2b72c7fe09501abf458
A4DEV_5831 <ahmadakanniluqman@gmail.com>
2026-05-03 20:21:13 +0100

    Add output module for displaying results and errors

---
+++ Grade_checker_modular/output_module.py
@@ -0,0 +1,8 @@
+# ─── Output Module ──────────────────────────────────────────────────────────
+
+def display_result(score, grade):
+    print(f"\nScore : {score}")
+    print(f"Grade : {grade}")
+
+def display_error(message):
+    print(f"Error : {message}")
